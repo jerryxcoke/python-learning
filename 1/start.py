9b156a84cbe396cc2a8587eccd888a8a1926c797
lkohari <lkohari@emarsys.com>
2019-02-12 16:52:21 +0100

    first lesson and init repo

---
+++ 1/start.py
@@ -0,0 +1,53 @@
+
+
+# print 'hello world'
+# print 'hello name'
+# name = 'name'
+# print 'hello ' + name
+
+# print 5
+# print 4+5
+# print 9*5
+# print 5**3
+
+# result = (4 + 5) / 3
+# print result
+# if 3 > 2:
+#     print 'ofc true'
+# else:
+#     print 'what happend'
+
+# if 3 >= 2:
+#     print '123' + 'ofc true'
+# else:
+#     print 'what happend'
+
+# result = 6 + 4
+# if result % 2 == 0 :
+#     print  str(result) + ' ofc true'
+# else:
+#     print 'what happend'
+
+
+# for value in range(0,10):
+#     print value * value
+
+# for value in range(0,10, 2):
+#     print value * value
+
+# for value in range(0,11,2):
+#     if  value == 2:
+#         print 'Two'
+#     else:
+#         print value * value
+
+for value in range(1,11):
+    if  value % 2 == 0:
+        print value
+    else:
+        if value % 3 == 0:
+            print value
+
+
+
+
