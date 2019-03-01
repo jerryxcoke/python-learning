a44bb752f5d109da704180e380e71a518da34e3a
lkohari <lkohari@emarsys.com>
2019-03-01 08:23:08 +0100

    hw

---
+++ 3/homework.py
@@ -0,0 +1,79 @@
+# megszamolas:
+# 2. Határozzuk meg az A(N) vektor negatív elemeinek számát!
+# 3. Számoljuk meg, hogy hányszor fordul elő egy szövegben az „a” névelő!
+# 4. Határozzuk meg az A(N) vektor azon pozitív elemeinek számát, amelyek
+# közvetlenül egy negatív elem után állnak!
+# 5. Adott az A(N) vektor és a K szám. Állapítsuk meg, hogy a vektor elemei köztt
+# hányszor szerepel a K!
+# 6. Állapítsuk meg, hogy az A(N) vektorban negatív vagy pozitív számból van-e több!
+# 7. Határozzuk meg az S karaktersorozatban a magánhangzók számát!
+
+
+# ---------------
+# maxker:
+# 2. 5 szám közül válasszuk ki a legnagyobbat és a legkisebbet!
+# 3. Határozzuk meg egy N elemű sorozat legkisebb pozitív elemét!
+# 4. Keressük meg az N elemű sorozatnak azt a legnagyobb elemét, amely nagyobb az
+# előtte lévő elemnél, de kisebb az őt követőnél!
+# 5. Egy folyó sodrára merőlegesen H méterenként megmértük a folyó mélységét.
+# Állapítsuk meg, hol a legmeredekebb a meder? Mekkora ez a meredekség?
+# 6. A rádió reggeli időjárás-jelentése alapján állapítszk meg, hogy az ország melyik
+# városában van a leghidegebb!
+
+# -----------------
+# osszegzes:
+# 1.Adjunk össze N darab szomszédos egész számot!
+# 2. Szorozzunk össze N darab szomszédos egész számot!
+# 3. Határozzuk meg K darab egész szám négyzetének összegét, és szorzatát!
+# 4. Határozzuk meg N darab szám átlagát!
+# 5. N hónapon át törlesztettünk egy kölcsönt, havonkénti fizetéssel. (Havonként
+# különböző összeget is fizethettünk). Adjuk meg az eddig kifizetett összeget!
+# 6. Adott egy tekéző sorozata (melyik fordulóban hány fát ütött). Írjunk programot,
+# amely meghatározza a versenyző összesített eredményét!
+
+
+# -------------------
+# eldontes:
+# 3. Írjunk programot, amely egy osztály nyilvántartásából megállapítja, hogy van-e év
+# vesztes az osztályban!
+# 4. Írjunk programot, amely egy szabászat személyi nyilvántartásából kideríti, hogy
+# dolgozik-e férfi ezen a munkaterületen!
+# 5. Adott egy 12. osztály tanulóinak nyilvántartása, valamint az enapi dátum. Írjunk
+# programot, amely megállapítja, hogy van-e olyan tanuló, aki nem nagykorú!
+# 6. Írjunk programot, amely egy adott értelmes szövegről eldönti, hogy több szóból
+# áll-e!
+# 7. Írjunk programot, amely egy adott értelmes szövegről eldönti, hogy több
+# mondatból áll-e!
+# 8. Ismert az egymást követő N nyáron át a lehullott átlagos csapadékmennyiség
+# (mm-ben). Ha 30 mm alatti érték van, akkor abban az évben központi öntözési
+# támogatást kapnak a gazdaságok. Kellett-e a vizsgált időszakban ilyen támogatást
+# adni?
+# 9. Ismert N autó fogyasztása. (100 km-ként fogyasztott literben mérve). Döntsük el,
+# hogy minden autó 10 liter alatt fogyasztott-e!
+
+
+# --------------
+# kivalasztas: (olyan kereses, ahol tudjuk, hogy letezik adott tulajdonsugu elem)
+# 2. Írjon programot, amely egy adott névsorban megmondja, hogy Kiss Pista hányadik
+# sorszámú!
+# 4. Határozzuk meg, hogy egy adott hónap melyik évszakba esik!
+# 5. Olvassunk be neveket addig, amíg nem írtunk egymás után két egyformát!
+# 7. Határozzuk meg, hogy hány nap múlva lesz a következő vasárnap!
+
+
+# ----------------------
+# kereses:
+# 4. A vasúti nyilvántartás tartalmazza a Savaria expresszre kiadott helyjegyeket. Írjunk
+# programot, amely meghatároz egy szabad helyet a vonaton!
+# 5. Adott egy táblázat, amelynek elemei karakterek (üres helyek is lehetnek benne).
+# Írjunk programot, amely választ a táblázatban egy véletlen helyet, majd kiválaszt
+# hozzá egy üres szomszédot!
+# 6. Egy házi telefonkönyv a nevek szerint rendezett. Keressünk meg benne egy adott
+# névhez tartozó telefonszámot!
+# 7. Egy csomag kártyát összekevertek.
+# a) Adjuk meg, hogy hol van egymás mellett két azonos színű lap!
+# b) Adjuk meg, hogy hol van egymás mellett két azonos figurájú lap!
+# c) Adjuk meg, hogy hol van benne joker (ha van benne egyáltalán)!
+# 8. Nyelvvizsgán a nyelvtani tesztek pontszámait ülési sorrendben jegyezték fel.
+# Keressünk olyan vizsgázót, aki ugyanannyi pontot kapott, mint a szomszédja!
+# (A tesztlapokat üléssorrendben szedték össze)
